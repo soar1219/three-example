82f75c15d0b7f9847cd59e2ea2609dd75af3863e
soar1219 <tsubasa1219.info@gmail.com>
2025-04-27 16:05:00 +0900

    ボールの表示

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,11 @@ import * as THREE from "three";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(50,window.innerWidth / window.innerHeight,0.1,1000);
+camera.position.set(0, 0, 500);
 const renderer = new THREE.WebGLRenderer({alpha : true});
 
 document.body.appendChild(renderer.domElement)
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.render(scene, camera);
 
 //ジオメトリの作成
 const ballGeometry = new THREE.SphereGeometry(100, 64, 32);
@@ -16,3 +15,6 @@ const ballMaterial = new THREE.MeshPhysicalMaterial();
 //メッシュ化
 const ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
 scene.add(ballMesh)
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
