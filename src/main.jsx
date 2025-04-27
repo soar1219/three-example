1a93026fc8e81361f723cdaac481f9faf68ae335
soar1219 <tsubasa1219.info@gmail.com>
2025-04-27 16:35:27 +0900

    光源を自動的に移動する

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+import { lightPosition } from "three/src/nodes/TSL.js";
+import { sin } from "three/tsl";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(50,window.innerWidth / window.innerHeight,0.1,1000);
@@ -16,5 +18,32 @@ const ballMaterial = new THREE.MeshPhysicalMaterial();
 const ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
 scene.add(ballMesh)
 
+const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+/* directionalLight.position.set(1, 1, 1) */
+/* scene.add(directionalLight) */
+
+const pointLight = new THREE.PointLight(0xffffff, 1);
+pointLight.position.set(-200, -200, -200)
+pointLight.decay = 1;
+pointLight.power = 1000;
+
+scene.add(pointLight)
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 30);
+scene.add(pointLightHelper);
+
+
+
+function animate() {
+  pointLight.position.set(
+    200 * Math.sin(Date.now() / 500),
+    200 * Math.sin(Date.now() / 1000),
+    200 * Math.cos(Date.now() / 500),
+  );
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera);
+}
+
+animate()
+
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.render(scene, camera);
