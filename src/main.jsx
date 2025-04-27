2503ec18acd58885fe2d4d6fee2549064ea37e62
soar1219 <tsubasa1219.info@gmail.com>
2025-04-27 15:11:54 +0900

    レンダラーの設定

--- src/main.jsx
+++ src/main.jsx
@@ -2,3 +2,9 @@ import * as THREE from "three";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(50,window.innerWidth / window.innerHeight,0.1,1000);
+const renderer = new THREE.WebGLRenderer({alpha : true});
+
+document.body.appendChild(renderer.domElement)
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
