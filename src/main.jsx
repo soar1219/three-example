62b80fdd9613616d90520b4ad33db8718881eaef
soar1219 <tsubasa1219.info@gmail.com>
2025-04-27 18:33:53 +0900

    地球の作成

--- src/main.jsx
+++ src/main.jsx
@@ -8,18 +8,18 @@ const renderer = new THREE.WebGLRenderer({alpha : true});
 
 document.body.appendChild(renderer.domElement)
 
-
+const texture = new THREE.TextureLoader().load("/src/assets/img/texture/earth.jpg")
 //ジオメトリの作成
 const ballGeometry = new THREE.SphereGeometry(100, 64, 32);
 //マテリアルの作成
-const ballMaterial = new THREE.MeshPhysicalMaterial();
+const ballMaterial = new THREE.MeshPhysicalMaterial({ map: texture});
 //メッシュ化
 const ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
 scene.add(ballMesh)
 
-/* const directionalLight = new THREE.DirectionalLight(0xffffff, 2); */
-/* directionalLight.position.set(1, 1, 1) */
-/* scene.add(directionalLight) */
+/* const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+directionalLight.position.set(1, 1, 1) 
+scene.add(directionalLight)  */
 
 const pointLight = new THREE.PointLight(0xffffff, 1);
 pointLight.position.set(-200, -200, -200)
@@ -33,6 +33,11 @@ scene.add(pointLightHelper);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
+function onWindowResize() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+}
 
 function animate() {
   pointLight.position.set(
@@ -44,6 +49,8 @@ function animate() {
   renderer.render(scene, camera);
 }
 
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio)
+window.addEventListener("resize", onWindowResize)
 animate()
 
-renderer.setSize(window.innerWidth, window.innerHeight);
