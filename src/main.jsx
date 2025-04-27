3a5690f14e1c2eec686fcd7f1cab3b905c68baf2
soar1219 <tsubasa1219.info@gmail.com>
2025-04-27 16:56:45 +0900

    マウスで動く

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import * as THREE from "three";
-import { lightPosition } from "three/src/nodes/TSL.js";
-import { sin } from "three/tsl";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(50,window.innerWidth / window.innerHeight,0.1,1000);
@@ -18,7 +17,7 @@ const ballMaterial = new THREE.MeshPhysicalMaterial();
 const ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
 scene.add(ballMesh)
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+/* const directionalLight = new THREE.DirectionalLight(0xffffff, 2); */
 /* directionalLight.position.set(1, 1, 1) */
 /* scene.add(directionalLight) */
 
@@ -32,6 +31,7 @@ scene.add(pointLight)
 const pointLightHelper = new THREE.PointLightHelper(pointLight, 30);
 scene.add(pointLightHelper);
 
+const controls = new OrbitControls(camera, renderer.domElement);
 
 
 function animate() {
